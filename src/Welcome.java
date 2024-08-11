fbd3fe6bd7b4060968b890fe552033b3e5845136
TaiBui2412 <23656541.tai@student.iuh.edu.vn>
2024-08-12 00:23:07 +0700

    TaiBui2412 23656541.tai@student.iuh.edu.vn

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Hello Bui Van Tai!");
+        System.out.print("Welcome to the DHKTPM19ATT ");
+    }
+}
